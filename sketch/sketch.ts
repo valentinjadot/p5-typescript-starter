12ca744f905e93a1b8e8a0db0b1a5d33b0cb23c9
Valentin <valentin@fintual.com>
2023-03-03 16:53:37 -0300

    chore(prettier) : fix prettier

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,48 +1,21 @@
 // GLOBAL VARS & TYPES
 let numberOfShapesControl: p5.Element;
 
-// P5 WILL AUTOMATICALLY USE GLOBAL MODE IF A DRAW() FUNCTION IS DEFINED
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function setup() {
-  console.log("🚀 - Setup initialized - P5 is running");
-
   createCanvas(windowWidth, windowHeight);
-  rectMode(CENTER).noFill().frameRate(30);
-  // NUMBER OF SHAPES SLIDER
-  numberOfShapesControl = createSlider(1, 30, 15, 1)
-    .position(10, 10)
-    .style("width", "100px");
+  background(0)
+
 }
 
-// p5 WILL AUTO RUN THIS FUNCTION IF THE BROWSER WINDOW SIZE CHANGES
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-// p5 WILL HANDLE REQUESTING ANIMATION FRAMES FROM THE BROWSER AND WIL RUN DRAW() EACH ANIMATION FROME
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function draw() {
-  // CLEAR BACKGROUND
-  background(0);
-
-  // CENTER OF SCREEN
-  translate(width / 2, height / 2);
-
-  const numberOfShapes = <number>numberOfShapesControl.value();
-  const colours = ColorHelper.getColorsArray(numberOfShapes);
-
-  // CONSISTENT SPEED REGARDLESS OF FRAMERATE
-  const speed = (frameCount / (numberOfShapes * 30)) * 2;
-
-  // DRAW ALL SHAPES
-  for (let i = 0; i < numberOfShapes; i++) {
-    push();
-    const lineWidth = 8;
-    const spin = speed * (numberOfShapes - i);
-    const numberOfSides = 3 + i;
-    const width = 40 * i;
-    strokeWeight(lineWidth);
-    stroke(colours[i]);
-    rotate(spin);
-    PolygonHelper.draw(numberOfSides, width);
-    pop();
-  }
+  console.log("he")
 }
